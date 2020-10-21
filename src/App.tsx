b31876b291db17c4936e514b74e73b8a5895bec5
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2020-10-21 03:35:04 +0200

    scale fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,4 @@
 import React from "react";
-import logo1 from "./assets/walkingVector/0.jpg";
-import logo2 from "./assets/walkingVector/1.jpg";
-import logo3 from "./assets/walkingVector/2.jpg";
-import logo4 from "./assets/walkingVector/3.jpg";
-import logo5 from "./assets/walkingVector/4.jpg";
-import logo6 from "./assets/walkingVector/5.jpg";
-import logo7 from "./assets/walkingVector/6.jpg";
-import logo8 from "./assets/walkingVector/7.jpg";
-import logo9 from "./assets/walkingVector/8.jpg";
 import pixelPc from "./assets/PixelPc.png";
 import pixelCup from "./assets/PixelCup.png";
 import plant from "./assets/Plant.gif";
@@ -19,77 +10,8 @@ import instagramLogo from "./assets/instagramLogo.png";
 
 import "./App.css";
 
-var countAvatar = 1;
-var avatarSrcAll = [
-  logo1,
-  logo2,
-  logo3,
-  logo4,
-  logo5,
-  logo6,
-  logo7,
-  logo8,
-  logo9,
-];
-var avatarSrc = logo1;
-// function List() {
-//   if (!countAvatar) {
-//     return null;
-//   }
-
-//   if (countAvatar === 1) {
-//     return <img src={logo1} className="App-logo" alt="logo" />;
-//   }
-//   if (countAvatar === 2) {
-//     return <img src={logo2} className="App-logo" alt="logo" />;
-//   }
-//   if (countAvatar === 3) {
-//     return <img src={logo3} className="App-logo" alt="logo" />;
-//   }
-//   if (countAvatar === 4) {
-//     return <img src={logo4} className="App-logo" alt="logo" />;
-//   }
-//   if (countAvatar === 5) {
-//     return <img src={logo5} className="App-logo" alt="logo" />;
-//   }
-//   if (countAvatar === 6) {
-//     return <img src={logo6} className="App-logo" alt="logo" />;
-//   }
-//   if (countAvatar === 7) {
-//     return <img src={logo7} className="App-logo" alt="logo" />;
-//   }
-//   if (countAvatar === 8) {
-//     return <img src={logo8} className="App-logo" alt="logo" />;
-//   }
-//   if (countAvatar === 9) {
-//     return <img src={logo9} className="App-logo" alt="logo" />;
-//   }
-// }
-
 class App extends React.Component<{}, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      avatarState: true,
-    };
-  }
-  tick() {
-    console.log(avatarSrc);
-    this.setState({ avatarState: false });
-    avatarSrc = avatarSrcAll[countAvatar];
-    ++countAvatar;
-    if (countAvatar > 8) {
-      countAvatar = 2;
-    }
-  }
 
-  componentDidMount() {
-    setInterval(() => this.tick(), 100);
-  }
-
-  componentWillUnmount() {
-    clearInterval();
-  }
   instagramImageClick = () => {
     window.open("https://www.instagram.com/tmilost/", "_blank");
   };
@@ -108,60 +30,49 @@ class App extends React.Component<{}, any> {
     return (
       <div className="App">
         <div className="mainDiv">
-          <img src={outSide} className="outSide" alt="plant" />
-          {/* <div className="top">
-          <div className="Frame">
-            <img src={frame} className="frame" alt="frame" />
-          </div>
-          <div className="Avatar">
-            <img src={avatarSrc} className="avatar" alt="logo" />
-
-            <p>
-              <b>
-                <code>Front-End Web Developer</code>
-              </b>
-            </p>
-          </div>
-        </div> */}
-
+          <img src={outSide} className="outSide" alt="outSide" />
+          <div className="table">
           <img src={plant} className="plant" alt="pixelPc" />
-          <img src={pixelPc} className="pixelPc" alt="pixelPc" />
-          <img src={pixelCup} className="pixelCup" alt="plant" />
-          <div className="PcScreen">
-          <div>
-             
-            </div>
-            <div>
-              <img
-                src={LinkedIn}
-                className="linkedIn"
-                alt="LinkedIn"
-                onClick={this.linkedinLink}
-              />
-            </div>
-            <div>
-              <img
-                src={gitHubLogo}
-                className="gitHubLogo"
-                alt="gitHubLogo"
-                onClick={this.gitHubImageClick}
-              /> <img
-              src={cvlogo}
-              className="cvlogo"
-              alt="cvlogo"
-              onClick={this.linkedinLink}
-            />
-            </div>
-            <div>
-              <img
-                src={instagramLogo}
-                className="instagramLogo"
-                alt="instagramLogo"
-                onClick={this.instagramImageClick}
-              />
-            </div>
+
+      
+            <img src={pixelPc} className="pixelPc" alt="pixelPc" />
+            {/* <div className="PcScreen">
+              <div>
+                <img
+                  src={LinkedIn}
+                  className="linkedIn"
+                  alt="LinkedIn"
+                  onClick={this.linkedinLink}
+                />
+              </div>
+              <div>
+                <img
+                  src={gitHubLogo}
+                  className="gitHubLogo"
+                  alt="gitHubLogo"
+                  onClick={this.gitHubImageClick}
+                />{" "}
+                <img
+                  src={cvlogo}
+                  className="cvlogo"
+                  alt="cvlogo"
+                  onClick={this.linkedinLink}
+                />
+              </div>
+              <div>
+                <img
+                  src={instagramLogo}
+                  className="instagramLogo"
+                  alt="instagramLogo"
+                  onClick={this.instagramImageClick}
+                />
+              </div> 
+            </div> */}
+                      <img src={pixelCup} className="pixelCup" alt="plant" />
           </div>
+   
         </div>
+        <div className="cvDiv">asdasd</div>
       </div>
     );
   }
