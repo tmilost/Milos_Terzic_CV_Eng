d3eb98f2fa7fc513927af980d8e33ff873ee2766
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2020-10-24 04:43:39 +0200

    add react-responsive-carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 import pixelPc from "./assets/PixelPc.png";
 import pixelCup from "./assets/PixelCup.png";
 import plant from "./assets/Plant.gif";
@@ -7,6 +9,9 @@ import cvlogo from "./assets/CVlogo.png";
 import LinkedIn from "./assets/LinkedIn.png";
 import gitHubLogo from "./assets/gitHubLogo.png";
 import instagramLogo from "./assets/instagramLogo.png";
+import cvPage1 from "./assets/Milos_Terzic_CV_Eng-1.jpg";
+import cvPage2 from "./assets/Milos_Terzic_CV_Eng-2.jpg";
+import cvPage3 from "./assets/Milos_Terzic_CV_Eng-3.jpg";
 
 import "./App.css";
 
@@ -65,7 +70,24 @@ class App extends React.Component<{}, any> {
             <img src={pixelCup} className="pixelCup" alt="plant" />
           </div>
         </div>
-        <div className="cvDiv">asdasd</div>
+        <div className="cvDiv">
+          <div className="slideShow">
+            <Carousel dynamicHeight={false}>
+              <div>
+                <img src={cvPage1} alt="CV Page 0" />
+                <p className="legend">Page 0</p>
+              </div>
+              <div>
+                <img src={cvPage2} alt="CV Page 1" />
+                <p className="legend">Page 1</p>
+              </div>
+              <div>
+                <img src={cvPage3} alt="CV Page 2" />
+                <p className="legend">Page 2</p>
+              </div>
+            </Carousel>{" "}
+          </div>
+        </div>
       </div>
     );
   }
